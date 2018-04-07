673cd46ce3542b63f6e9f4696fc9003bcc128092
kmwdredden <kmwdredden@gmail.com>
2018-04-06 21:45:47 -0400

    Initial Commit

---
+++ MYPaddle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MYPaddle
+//
+//  Created by Katrice Williams-Dredden on 4/6/18.
+//  Copyright © 2018 Katrice WilliamsDredden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
